e0daf8f90ba7d03e2b1bf3273cbb6f3da5db9b24
Victor Rachieru <vick91r@gmail.com>
2016-03-28 21:50:18 +0300

    Add configuration module

---
+++ configuration.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from json import load
+
+configurationFile = 'config.json'
+
+class Configuration(object):
+	def __init__(self, obj = None):
+		self._obj = self.load() if obj is None else obj
+
+	def load(self):
+		with open(configurationFile, 'r') as data:
+			return load(data)
+
+	def __getattr__(self, key):
+		try:
+			return Configuration(self._obj[key])
+		except (KeyError, AttributeError, IndexError, TypeError):
+			return None
+
+	__getitem__ = __getattr__
+
+	def __repr__(self):
+		return self._obj
