f5b4f8e38a7061fadbee50b19f48fc64e1b9ec36
Victor Rachieru <vick91r@gmail.com>
2016-03-28 21:50:45 +0300

    Add twitter module

---
+++ twitter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+from tweepy import OAuthHandler, API
+
+class Twitter():
+	def __init__(self, config = None):
+		auth = OAuthHandler(unicode(config.consumerKey), unicode(config.consumerSecret))
+		auth.set_access_token(unicode(config.accessToken), unicode(config.accessTokenSecret))
+		self._api = API(auth)
+
+	def tweet(self, message):
+		self._api.update_status(message)
+ 
