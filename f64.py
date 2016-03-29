d4b87215e9d3795fc78004b41e7a56e6c33a1347
Victor Rachieru <vick91r@gmail.com>
2016-03-29 19:52:01 +0300

    Refactor F64 parsing into module

--- f64.py
+++ f64.py
@@ -1,104 +1,60 @@
-from time import gmtime, strftime
+#!/usr/bin/env python
+
 from lxml import html
-import requests
-import json
+from requests import get
 
-categories = {
-	"lenses" : "http://www.f64.ro/obiective-foto-second-hand.html?Montura[]=Nikon", 
-	"accessories" : "http://www.f64.ro/blitz-uri-lumini-studio-accesorii-second-hand.html"
-}
+BASE_URL = "http://www.f64.ro"
+PER_PAGE = 1000
+CATEGORIES = [
+	"aparate-foto-second-hand",
+	"obiective-foto-second-hand",
+	"blitz-uri-lumini-studio-accesorii-second-hand"
+]
 
-class Html():
-	@staticmethod
-	def read(url):
-		return html.fromstring(requests.get(url).content)
+class F64():
+	products = []
 
+	def __init__(self):
+		map(self.parse, CATEGORIES)
 
-class Category():
-	@staticmethod
-	def displayProducts(products):
-		for product in products:
-			print Category.getProduct(product)
+	def parse(self, category):
+		url = self.getCategoryUrl(category)
+		content = self.getPageContent(url)
+		products = self.parseProducts(content)
+		map(lambda p : self.products.append(self.parseProduct(p)), products)
 
-	@staticmethod
-	def getProducts(dom):
+	def getCategoryUrl(self, category):
+		return "%s/%s.html?per_page=%s" % (BASE_URL, category, PER_PAGE)
+
+	def getPageContent(self, url):
+		return html.fromstring(get(url).content)
+
+	def parseProducts(self, dom):
 		return dom.xpath('//div[@class="product_list_container"]')
 
-	@staticmethod
-	def getProduct(product):
-		return { 'product' : Category.getProductCode(product), 'title' : Category.getTitle(product), 'price' : Category.getPrice(product) }
-	
-	@staticmethod
-	def getTitle(product):
+	def parseProduct(self, product):
+		title = self.parseProductTitle(product)
+		code = self.parseProductCode(product)
+		price = self.parseProductTitle(product)
+		url = self.parseProductUrl(product)
+		return Product(title, code, price, url)
+
+	def parseProductUrl(self, product):
+		return product.xpath('.//td[@class="product_title"]//a/@href')[0]
+
+	def parseProductTitle(self, product):
 		return product.xpath('.//h2/text()')[0]
 
-	@staticmethod
-	def getProductCode(product):
+	def parseProductCode(self, product):
 		return product.xpath('.//div[@class="stars_cod_product"]//b/text()')[0]
 
-	@staticmethod
-	def getPrice(product):
+	def parseProductPrice(self, product):
 		return product.xpath('.//span[@class="price_list_int"]/text()')[0] + "," + product.xpath('.//sup[@class="price_list_dec"]/text()')[0] + " " + product.xpath('.//span[@class="price_list_currency"]/text()')[0]
 
 
-class Log():
-	@staticmethod
-	def write(data):
-		with open("f64.log", "a") as f:
-			f.write(strftime('%Y-%m-%d %H:%M:%S', gmtime()) + " : " + data + "\n")
-
-
-class Database():
-	data = [];
-
-	def __init__(self):
-		self.read()
-
-	def read(self):
-		with open('f64.db', 'r') as f:
-			self.data = json.load(f)
-
-	def write(self):
-		with open('f64.db', 'w') as f:
-			f.write(json.dumps(self.data, sort_keys=True, indent=2))
-
-	def productExists(self, product):
-		return (True, False)[self.getProduct(product) == None]
-
-	def getProduct(self, product):
-		for item in self.data:
-			if item['product'] == product:
-				return item
-		return None
-
-	def addProduct(self, code, title, price):
-		self.data.append({ 'product' : code, 'title' : title, 'price' : { strftime('%Y-%m-%d %H:%M:%S', gmtime()) : price }})
-		Log.write("New product [" + code + "] " + title + " @ " + price)
-
-	def updatePrice(self, product, price):
-		if self.getLatestPrice(product) != price:
-			self.addNewPrice(product, price)
-			Log.write(product + " price updated from " + self.getLatestPrice(product) + " to " + price)
-
-	def getLatestPrice(self, product):
-		return sorted(self.getProduct(product)['price'].items())[-1][1]
-
-	def addNewPrice(self, product, price):
-		self.getProduct(product)['price'].update({ strftime('%Y-%m-%d %H:%M:%S', gmtime()) : price })
-
-
-def main():
-	database = Database()
-
-	for name, url in categories.items():
-		products = Category.getProducts(Html.read(url))
-		for product in products:
-			p = Category.getProduct(product)
-			if database.productExists(p['product']) == False:
-				database.addProduct(p['product'], p['title'], p['price'])
-			else:
-				database.updatePrice(p['product'], p['price'])
-
-	database.write()
-
-main()
+class Product():
+	def __init__(self, title, code, price, url):
+		self.title = title
+		self.code = code
+		self.price = price
+		self.url = url
